820c80a72ce131bc3acc8d11f895923b5936e5ee
jeffbaraton <jean-francois.baraton@elisa.fi>
2025-04-04 17:25:11 +0300

    broken meta ball
    https://github.com/enzomanuelmangano/exploring-skia/blob/main/metaball/App.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -47,6 +47,27 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="metaball"
+        options={{
+          title: 'Meta ball',
+          tabBarIcon: ({ color }) => <TabBarIcon name="file-text-o" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/metaball.tsx
@@ -0,0 +1,88 @@
+import { StatusBar, useWindowDimensions } from 'react-native';
+import React, { useMemo } from 'react';
+import {
+    Blur,
+    Circle,
+    ColorMatrix,
+    Group,
+    Paint,
+    SweepGradient,
+    vec,
+} from '@shopify/react-native-skia';
+import { useSharedValue, withSpring } from 'react-native-reanimated';
+import Touchable, { useGestureHandler } from 'react-native-skia-gesture';
+import {View} from '@/components/Themed';
+
+const RADIUS = 80;
+
+
+export default function TabTwoScreen() {
+    const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+
+    const cx = useSharedValue(windowWidth / 2);
+    const cy = useSharedValue(windowHeight / 2);
+
+    const context = useSharedValue({
+        x: 0,
+        y: 0,
+    });
+
+    const gestureHandler = useGestureHandler({
+        onStart: () => {
+            'worklet';
+            context.value = {
+                x: cx.value,
+                y: cy.value,
+            };
+        },
+        onActive: ({ translationX, translationY }) => {
+            'worklet';
+            cx.value = translationX + context.value.x;
+            cy.value = translationY + context.value.y;
+        },
+        onEnd: () => {
+            'worklet';
+            cx.value = withSpring(windowWidth / 2);
+            cy.value = withSpring(windowHeight / 2);
+        },
+    });
+
+    const layer = useMemo(() => {
+        return (
+            <Paint>
+                {/* pixelOpacity > blurredOpacity * 60 - 30 */}
+                <Blur blur={30} />
+                <ColorMatrix
+                    matrix={[
+                        // R, G, B, A, Bias (Offset)
+                        // prettier-ignore
+                        1, 0, 0, 0, 0,
+                        // prettier-ignore
+                        0, 1, 0, 0, 0,
+                        // prettier-ignore
+                        0, 0, 1, 0, 0,
+                        // prettier-ignore
+                        0, 0, 0, 60, -30,
+                    ]}
+                />
+            </Paint>
+        );
+    }, []);
+    return (
+        <>
+            <StatusBar barStyle={'light-content'} />
+            <Touchable.Canvas
+                style={{
+                    flex: 1,
+                    backgroundColor: '#111',
+                }}
+            >
+                <Group layer={layer}>
+                    <Touchable.Circle {...gestureHandler} cx={cx} cy={cy} r={RADIUS} />
+                    <Circle cx={windowWidth / 2} cy={windowHeight / 2} r={RADIUS} />
+                    <SweepGradient c={vec(0, 0)} colors={['cyan', 'magenta', 'cyan']} />
+                </Group>
+            </Touchable.Canvas>
+        </>
+    );
+}
