eb0706f06e7a20e76c285e4ed958a4288904e61d
jfbaraton <jf.baraton@gmail.com>
2025-04-05 14:32:23 +0300

    image v2

--- app/(tabs)/metaball.tsx
+++ app/(tabs)/metaball.tsx
@@ -1,44 +1,53 @@
 import { useState } from 'react';
-import { useImage,Image } from 'expo-image';
 
 import { useWindowDimensions, StyleSheet, View  } from 'react-native';
-import {Canvas,Text, Circle, Group} from "@shopify/react-native-skia";
+import {Canvas,Text, Circle, Group, useImage, Image} from "@shopify/react-native-skia";
 import {Star} from '@/components/Star';
 import { Asset } from 'expo-asset';
-//const IMAGE = Asset.fromModule(require('../../assets/sprites/ball.png'));
+import BallFront from '@/assets/sprites/ball.png';
+import FieldFront from '@/assets/sprites/field.jpg';
 
 export default function TabTwoScreen() {
-    const { width, height } = useWindowDimensions();
-    const image = useImage('../../assets/assets/sprites/ball.png', {
-        maxWidth: 800,
-        onError(error, retry) {
-          console.error('Loading failed:', error.message);
-        }
-      });
+    //const { width, height } = useWindowDimensions();
+    const width = 520;
+    const height = 420;
 
-    console.log("dims ", width, height)
+    const ball = useImage(BallFront.uri, {
+         maxWidth: 800,
+         onError(error, retry) {
+           console.error('Loading failed:', error.message);
+         }
+    });
+    const field = useImage(FieldFront.uri, {
+         maxWidth: 800,
+         onError(error, retry) {
+           console.error('Loading failed:', error.message);
+         }
+    });
 
-   //const bg = useImage(require('./../../assets/sprites/field.jpg'));
-   //const bg = useImage(require("@/assets/sprites/field.jpg"));
-    //const ball = useImage(require('../../assets/sprites/ball.png'));
- ///  transform={[{ rotate: -90 }]}
- // <Image image={bg} width={width} height={height}  fit={'cover'}/>
-    const r = width/2 * 0.33;
-    if (!image) {
+    //console.log("BallFront ", JSON.stringify(BallFront))
+
+    if (!ball || !field) {
         return <Text>Image is loading...</Text>;
     }
-    console.log("img ", image.width, image.height, image.localUri, image.uri)
     return (
         <View style={styles.container}>
+            <Canvas style={{ width, height }}>
                <Image
-                    source={"http://localhost:8081/assets/assets/sprites/ball.png"}
-
-                    style={styles.image}
-                    contentFit="cover"
-                    transition={1000}
+                    image={field}
+                    width={520}
+                    height={220}
+                    fit={'cover'}
 
                />
+               <Image
+                    image={ball}
+                    width={120}
+                    height={120}
+                    fit={'cover'}
 
+               />
+            </Canvas>
         </View>
     );
 }
