ab768fb1feb0e2cf4d41af8d8e28c6163e9d56e3
jfbaraton <jf.baraton@gmail.com>
2025-04-06 12:28:14 +0300

    click to move the ball

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { useWindowDimensions, StyleSheet, View  } from 'react-native';
 import {Canvas,Text, Circle, Group, useImage, Image} from "@shopify/react-native-skia";
+import {useSharedValue, withTiming} from "react-native-reanimated";
+import { GestureDetector, Gesture } from "react-native-gesture-handler";
+
 import {Star} from '@/components/Star';
 import { Asset } from 'expo-asset';
 import BallFront from '@/assets/sprites/ball.png';
@@ -11,6 +14,10 @@ export default function TabTwoScreen() {
     //const { width, height } = useWindowDimensions();
     const width = 720;
     const height = 370;
+    const ballsize = width/10;
+
+    const validateBallX = (oneBallX) => Math.min(Math.max(0,oneBallX-ballsize/2), width-ballsize)
+    const validateBallY = (oneBallY) => Math.min(Math.max(0,oneBallY-ballsize/2), height-ballsize)
 
     const ball = useImage(BallFront.uri, {
          maxWidth: 800,
@@ -25,29 +32,45 @@ export default function TabTwoScreen() {
          }
     });
 
+    const ballX = useSharedValue(validateBallX(width/7))
+    const ballY = useSharedValue(validateBallY(height/2))
     //console.log("BallFront ", JSON.stringify(BallFront))
+
+    useEffect(() => {
+        ballX.value = withTiming(validateBallX(width/2),{ duration : 1000});
+    });
+
+    const onFieldTouch = (event) => {
+        console.log("touch ", event)
+        ballX.value = withTiming(validateBallX(event.x),{ duration : 1000});
+        ballY.value = withTiming(validateBallX(event.y),{ duration : 1000});
+    }
+    const gestureTap = Gesture.Tap().onStart(onFieldTouch);
 
     if (!ball || !field) {
         return <Text>Image is loading...</Text>;
     }
     return (
         <View style={styles.container}>
-            <Canvas style={{ width, height }}>
-               <Image
-                    image={field}
-                    width={width}
-                    height={height}
-                    fit={'cover'}
+            <GestureDetector gesture={gestureTap}>
+                <Canvas style={{ width, height }} >
+                   <Image
+                        image={field}
+                        width={width}
+                        height={height}
+                        fit={'cover'}
 
-               />
-               <Image
-                    image={ball}
-                    width={width/10}
-                    height={width/10}
-                    fit={'cover'}
-
-               />
-            </Canvas>
+                   />
+                   <Image
+                        image={ball}
+                        width={ballsize}
+                        height={ballsize}
+                        fit={'cover'}
+                        x={ballX}
+                        y={ballY}
+                   />
+                </Canvas>
+            </GestureDetector>
         </View>
     );
 }
